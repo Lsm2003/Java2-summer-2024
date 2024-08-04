aa8cce129c4bffd126fe0c829f3dc7663c844b3f
Lsm2003 <Lucasmaloney2003@gmail.com>
2024-08-04 19:16:35 -0230

    Project work

--- src/main/java/Project/Hand.java
+++ src/main/java/Project/Hand.java
@@ -1,4 +1,43 @@
 package Project;
 
+import java.util.ArrayList;
+
+
 public class Hand {
+    public ArrayList<PlayingCard> cards;
+
+    public Hand(ArrayList<PlayingCard> cards) {
+        this.cards = cards;
+
+    }
+
+    public void addCard(PlayingCard card){
+        this.cards.add(card);
+    }
+
+    public int getScore() {
+        int score = 0;
+        for (PlayingCard card: this.cards) {
+            if (card.getValue() > 10) {
+                score += 10;
+            }
+            else if (card.getValue() == 1 && score <= 10) {
+                score += 11;
+            }
+            else {
+                score += card.getValue();
+            }
+
+        }
+        return score;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder cardsasString = new StringBuilder();
+        for (PlayingCard card : this.cards) {
+            cardsasString.append(card.toString());
+        }
+        return cardsasString.toString();
+    }
 }
--- src/main/java/Project/Player.java
+++ src/main/java/Project/Player.java
@@ -1,4 +1,18 @@
 package Project;
 
 public class Player {
+    public int chips;
+
+    public Player(int chips) {
+        this.chips = chips;
+    }
+
+    public int bet(int bet) {
+        if (bet < this.chips) {
+            this.chips =- bet;
+            return bet;
+
+        }
+        else return 0;
+    }
 }
